d0a4de5cf7504265d8b0328e8ab930232698a3e9
pcdhamejani <pcdhamejani74@gmail.com>
2024-03-31 11:46:15 +0530

    contact form updated

--- scripts/form.js
+++ scripts/form.js
@@ -1,6 +1,6 @@
 var x = document.getElementById("form_sample");
 var createform = document.createElement("form"); // Create New Element Form
-createform.setAttribute("action", ""); // Setting Action Attribute on Form
+createform.setAttribute("action", "clothing.html"); // Setting Action Attribute on Form
 createform.setAttribute("method", "post"); // Setting Method Attribute on Form
 x.appendChild(createform);
 
